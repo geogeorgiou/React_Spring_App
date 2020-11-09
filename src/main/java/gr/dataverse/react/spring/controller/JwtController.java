2a1e20edf02f5d516d0b1aa51fab8e58187dd76a
g.georgiou <g.georgiou@dataverse.gr>
2020-11-09 12:24:03 +0200

    Adding jwt header handler util in backend

--- src/main/java/gr/dataverse/react/spring/controller/JwtController.java
+++ src/main/java/gr/dataverse/react/spring/controller/JwtController.java
@@ -2,19 +2,24 @@ package gr.dataverse.react.spring.controller;
 
 import gr.dataverse.react.spring.json.JwtRequest;
 import gr.dataverse.react.spring.util.JwtUtil;
+import gr.dataverse.react.spring.util.UserAuthUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api/jwt")
 public class JwtController {
 
+    //remove this when done (logic goes into UserAuth)
     @Autowired
     private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserAuthUtil userAuth;
 
     @PostMapping("/enc")
     public ResponseEntity<?> encodeJWT(@RequestBody JwtRequest request) {
@@ -30,6 +35,13 @@ public class JwtController {
     @PostMapping("/dec")
     public ResponseEntity<?> decodeJWT(@RequestBody String jwt){
         return ResponseEntity.ok(jwtUtil.decodeJWT(jwt));
+    }
+
+    @PostMapping("/userAuth")
+    public ResponseEntity<?> isUserAuth(HttpServletRequest request, HttpServletResponse response) {
+
+
+        return ResponseEntity.ok("valid="+userAuth.isUserAuth(request));
     }
 
 
---
+++ src/main/java/gr/dataverse/react/spring/util/UserAuthUtil.java
@@ -0,0 +1,51 @@
+package gr.dataverse.react.spring.util;
+
+
+import gr.dataverse.react.spring.filter.ConsoleLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class UserAuthUtil {
+
+    //use this to validate what's coming from request
+    @Autowired
+    JwtUtil jwtUtil;
+
+    static Logger logger = LoggerFactory.getLogger(ConsoleLogger.class);
+
+    //Basic authentication Util that returns the user credentials in String Array
+
+    public boolean isUserAuth(HttpServletRequest httpRequest){
+
+
+        try{
+
+            final String authorization = httpRequest.getHeader("Authorization");
+//            logger.debug("_**AUTHORIZATION**_"+authorization);
+
+            //Authorization that is being applied (using jwt token)
+            if (authorization != null) {
+//                System.out.println("_**AUTHORIZATION**_"+authorization);
+//
+//                logger.debug("_*********_");
+                String jwt = authorization.substring("Bearer".length()).trim();
+//                logger.debug(jwt);
+
+                jwtUtil.decodeJWT(jwt);
+                return true;
+
+            }
+
+        }catch (Exception e){
+            return false;
+        }
+
+        return false;
+    }
+
+}
